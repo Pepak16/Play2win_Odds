e9e45bca32f2a9847ab1280007efc9ed4f7b9c67
Persha <pepak16@student.sdu.dk>
2019-07-29 01:13:31 +0200

    Created project

---
+++ Play2win_Odds_solution/Play2win_Odds/Play2win_Odds/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Play2win_Odds.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Play2win_Odds_solution/Play2win_Odds/Play2win_Odds/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Play2win_Odds.Models;
+
+namespace Play2win_Odds.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
